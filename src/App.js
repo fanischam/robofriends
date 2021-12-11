ea246f6cead526e2500864b19638a2025eebef4a
fanischam <fchamalidis@yahoo.com>
2021-12-11 22:23:08 +0200

    Removed hardcoded robots and used fetch from an API to get them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Component } from "react/cjs/react.development";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 import "./App.css";
 
@@ -9,10 +8,17 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: "",
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
   onSearchChange = (e) => {
     this.setState({ searchField: e.target.value });
   };
@@ -26,7 +32,11 @@ class App extends Component {
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+        {this.state.robots.length === 0 ? (
+          <h1>Loading...</h1>
+        ) : (
+          <CardList robots={filteredRobots} />
+        )}
       </div>
     );
   }
