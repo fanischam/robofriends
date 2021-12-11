60adbb8c215606d5c373d49af7fdd2a121ea34cc
fanischam <fchamalidis@yahoo.com>
2021-12-11 14:50:11 +0200

    Added title and SearchBox

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Component } from "react/cjs/react.development";
+import CardList from "./CardList";
+import { robots } from "./robots";
+import SearchBox from "./SearchBox";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: "",
+    };
+  }
+  onSearchChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
+
+export default App;
