01688941973b59e0dd5e74c50ebae97279bc8e65
fanischam <fchamalidis@yahoo.com>
2021-12-12 18:20:42 +0200

    Made code look a bit prettier and divided files into components and containers folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Component } from "react/cjs/react.development";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
 import "./App.css";
-import Scroll from "./Scroll";
+import Scroll from "../components/Scroll";
 
 class App extends Component {
   constructor() {
@@ -24,16 +24,15 @@ class App extends Component {
     this.setState({ searchField: e.target.value });
   };
   render() {
-    const filteredRobots = this.state.robots.filter((robots) => {
-      return robots.name
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
+    const { searchField, robots } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        {this.state.robots.length === 0 ? (
+        {!robots.length ? (
           <h1>Loading...</h1>
         ) : (
           <Scroll>
